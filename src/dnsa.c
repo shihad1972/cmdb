261b8ec698363f543f8af2381616db80b9793324
Iain M Conochie <iain@shihad.org>
2012-10-29 20:34:52 +0000

    oops. Removed domain from dnsa.c. This has been added back in now

--- src/dnsa.c
+++ src/dnsa.c
@@ -18,31 +18,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <mysql.h>
 #include "dnsa.h"
 #include "write_zone.h"
 #include "rev_zone.h"
+#include "mysql_funs.h"
 
 int main(int argc, char *argv[])
 {
+	MYSQL dnsa;
+	MYSQL_ROW dnsa_row;
+	MYSQL_RES *dnsa_res;
 	comm_line_t command;
-	char *domain, dom[CONF_S], config[CHKC + 1][CONF_S];
+	mysql_query_data_t *mydqp;
+	my_ulonglong dnsa_rows;
+	char *domain, *queryp, dom[CONF_S], config[CHKC + 1][CONF_S];
+	const char *dquery, *unix_socket;
 	int retval, id;
+	unsigned int port = 3306;
+	unsigned long int client_flag = 0;
 
+	/* Get command line args. See above */
 	retval = parse_command_line(argc, argv, &command);
 	if (retval < 0) {
 		printf("Usage: %s [-d | -w] [-f | -r] -n <domain/netrange>\n",
 			       argv[0]);
 		exit (retval);
 	}
+	/* Get config values from config file */
 	sprintf(config[CONFIGFILE], "/etc/dnsa/dnsa.conf");
 	retval = parse_config_file(config);
 	if (retval < 0) {
 		printf("Config file parsing failed! Using default values\n");
 	}
-	
-	strncpy(dom, command.domain, CONF_S);
-	domain = dom;
-	
+	domain = &command.domain;
 	if ((strncmp(command.action, "write", COMM_S) == 0)) {
 		if ((strncmp(command.type, "forward", COMM_S) == 0)) {
 			wzf(domain, config);
