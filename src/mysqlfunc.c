b8848682b9709dbfa9d8625ccbbd955308f42a07
Iain M Conochie <iain@shihad.org>
2012-11-22 11:43:48 +0000

    Updated error_code in mysqlfunc.c to be my_error_code to stop double pointer free

--- src/mysqlfunc.c
+++ src/mysqlfunc.c
@@ -15,41 +15,41 @@
 #include "mysqlfunc.h"
 
 const char *error_string;
-char *error_code;
+char *my_error_code;
 
 void dnsa_mysql_init(dnsa_config_t *dc, MYSQL *dnsa_mysql)
 {
 	const char *unix_socket;
 	
-	if (!(error_code = calloc(RBUFF_S, sizeof(char))))
-		report_error(MALLOC_FAIL, "error_code in dnsa_mysql_init");
+	if (!(my_error_code = calloc(RBUFF_S, sizeof(char))))
+		report_error(MALLOC_FAIL, "my_error_code in dnsa_mysql_init");
 	
 	unix_socket = dc->socket;
-	error_string = error_code;
+	error_string = my_error_code;
 	
 	if (!(mysql_init(dnsa_mysql))) {
-		sprintf(error_code, "none");
+		sprintf(my_error_code, "none");
 		report_error(MY_INIT_FAIL, error_string);
 	}
 	if (!(mysql_real_connect(dnsa_mysql, dc->host, dc->user, dc->pass,
 		dc->db, dc->port, unix_socket, dc->cliflag)))
 		report_error(MY_CONN_FAIL, mysql_error(dnsa_mysql));
 	
-	free(error_code);
+	free(my_error_code);
 }
 
 void dnsa_mysql_query(MYSQL *mydnsa, const char *query)
 {
 	int error;
 	
-	if (!(error_code = calloc(RBUFF_S, sizeof(char))))
-		report_error(MALLOC_FAIL, "error_code in dnsa_mysql_init");
+	if (!(my_error_code = calloc(RBUFF_S, sizeof(char))))
+		report_error(MALLOC_FAIL, "my_error_code in dnsa_mysql_init");
 	
-	error_string = error_code;
+	error_string = my_error_code;
 	
 	error = mysql_query(mydnsa, query);
 	if ((error != 0)) {
 		report_error(MY_QUERY_FAIL, error_string);
 	}
-	free(error_code);
+	free(my_error_code);
 }
