3566751702326c3a3020d7a99f973a991e7d0dec
Iain M Conochie <iain@shihad.org>
2012-12-11 16:43:44 +0000

    Database updates. Needed for next commit

--- conf/database-update.sql
+++ conf/database-update.sql
@@ -19,4 +19,17 @@ INSERT INTO configuration (config, value) VALUES ("cbctlos", "/usr/local/build/"
 INSERT INTO configuration (config, value) VALUES ("cbcdhcp", "/etc/dhcp/dhcpd.hosts");
 INSERT INTO configuration (config, value) VALUES ("cbcpreseed", "preseed/");
 INSERT INTO configuration (config, value) VALUES ("cbckickstart", "ks/");
-
+UPDATE boot_line SET boot_line = "locale=en_GB.UTF-8 keymap=uk auto=true priority=critical vga=788" WHERE os = 'debian' and os_ver = '6';
+UPDATE boot_line SET boot_line = "auto=true priority=critical vga=788" WHERE os = 'debian' and os_ver = '5';
+UPDATE boot_line SET boot_line = "ksdevice=eth0 console=tty0 ramdisk_size=8192" WHERE os = 'centos';
+UPDATE boot_line SET boot_line = "ksdevice=eth0 console=tty0 ramdisk_size=8192" WHERE os = 'redhat';
+UPDATE boot_line SET boot_line = "country=GB console-setup/layoutcode=gb auto=true priority=critical vga=788" WHERE os = 'ubuntu';
+UPDATE build_os bo, boot_line bl SET bo.boot_id = bl.boot_id WHERE bo.alias = 'redhat' and bo.os_version = '6.0' AND bl.os = 'redhat';
+UPDATE build_os SET ver_alias = 'none' WHERE alias = 'centos' OR alias='redhat' OR alias= 'slack';
+ALTER TABLE build_os MODIFY ver_alias varchar(25) NOT NULL DEFAULT 'none';
+ALTER TABLE boot_line MODIFY COLUMN boot_line varchar(150) NOT NULL DEFAULT 'none';
+UPDATE boot_line SET boot_line = 'none' WHERE os = 'slack';
+ALTER TABLE build_type MODIFY COLUMN arg varchar(16) NOT NULL DEFAULT 'none';
+ALTER TABLE build_type MODIFY COLUMN url varchar(80) NOT NULL DEFAULT 'none';
+UPDATE build_type SET build_type = 'none', arg = 'none' WHERE alias = 'slack';
+ALTER TABLE build_type MODIFY COLUMN build_type varchar(25) NOT NULL DEFAULT 'none';
