b50fc880956366f7b57c7ff94b283f903f063a4e
Iain M Conochie <iain@shihad.org>
2013-01-31 12:37:31 +0000

    Added license information to the dnsa source files

--- src/dnsa.c
+++ src/dnsa.c
@@ -1,21 +1,24 @@
 /* 
  * 
- * dnsa.c: First attempt to write a multi function command line program
- * for dnsa.
- * Command line args:
- * 
- * -d :display
- * -w :write
- * -c :write configuration
- * -l :list zones in database.
- *   **At least one of these must be present**
- * -f : forward zone
- * -r : Reverse zone
- *   **One of these must be present but not both**
- * -n : domain name or netblock. only /8 /16 /24 netblocks accepted
- *      needed for -d and -w functionality
+ *  dnsa: DNS Administration
+ *  Copyright (C) 2012 - 2013  Iain M Conochie <iain-AT-thargoid.co.uk>
  *
- * (C) 2012 Iain M Conochie
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *   
+ * dnsa.c: main() function for the dnsa program
+ *
  * 
  */
 
--- src/fwd_zone.c
+++ src/fwd_zone.c
@@ -1,12 +1,31 @@
-/* fwd_zone:
+/* 
+ *
+ *  dnsa: DNS Administration
+ *  Copyright (C) 2012 - 2013  Iain M Conochie <iain-AT-thargoid.co.uk>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ *  fwd_zone:
  * 
- * Contains various funtions that are needed to import / export
- * forward zone information from the database, and to also write
- * the zone files and BIND configuration files.
+ *  Contains various funtions that are needed to import / export
+ *  forward zone information from the database, and to also write
+ *  the zone files and BIND configuration files.
  * 
- * Part of the DNSA program
+ *  Part of the DNSA program
  * 
- * (C) Iain M Conochie 2012 - 2013
+ *  (C) Iain M Conochie 2012 - 2013
  * 
  */
 
