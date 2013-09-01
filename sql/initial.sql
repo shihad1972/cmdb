1bfd0fba5fbc497a40c6898015da41cb83344138
Iain M Conochie <iain@thargoid.co.uk>
2013-09-01 08:42:02 +0100

    Added hobbit-client and lib(nss|pam)-ldap to package lists
    Debian and Ubuntu now have the ldap and hobbit-client packages added

--- sql/initial.sql
+++ sql/initial.sql
@@ -39,8 +39,10 @@ INSERT INTO packages (package, varient_id, os_id) SELECT "nfs4-acl-tools", v.var
 INSERT INTO packages (package, varient_id, os_id) SELECT "less", v.varient_id, o.os_id FROM varient v, build_os o;
 INSERT INTO packages (package, varient_id, os_id) SELECT "locate", v.varient_id, o.os_id FROM varient v, build_os o;
 ;
-INSERT INTO packages (package, varient_id, os_id) SELECT "ldap-utils", v.varient_id, o.os_id FROM varient v, build_os o WHERE o.alias = "debian";
-INSERT INTO packages (package, varient_id, os_id) SELECT "ldap-utils", v.varient_id, o.os_id FROM varient v, build_os o WHERE o.alias = "ubuntu";
+INSERT INTO packages (package, varient_id, os_id) SELECT "ldap-utils", v.varient_id, o.os_id FROM varient v, build_os o WHERE o.alias IN ("debian", "ubuntu");
+INSERT INTO packages (package, varient_id, os_id) SELECT "libnss-ldap", v.varient_id, o.os_id FROM varient v, build_os o WHERE o.alias IN ("debian", "ubuntu");
+INSERT INTO packages (package, varient_id, os_id) SELECT "libpam-ldap", v.varient_id, o.os_id FROM varient v, build_os o WHERE o.alias IN ("debian", "ubuntu");
+INSERT INTO packages (package, varient_id, os_id) SELECT "hobbit-client", v.varient_id, o.os_id FROM varient v, build_os o WHERE o.alias IN ("debian", "ubuntu");
 INSERT INTO packages (package, varient_id, os_id) SELECT "openldap", v.varient_id, o.os_id FROM varient v, build_os o WHERE o.alias = "centos";
 INSERT INTO packages (package, varient_id, os_id) SELECT "openldap-clients", v.varient_id, o.os_id FROM varient v, build_os o WHERE o.alias = "centos";
 ;
