e12f1a9caee92cb747e8104a3feb0b1836c76a15
Iain M Conochie <iain@thargoid.co.uk>
2013-09-14 14:45:29 +0100

    Updates for the initialisation of the database SQL files

--- sql/initial.sql
+++ sql/initial.sql
@@ -1,7 +1,4 @@
--- Create build_type entries
---INSERT INTO build_type (alias, build_type, arg, url, mirror, boot_line) VALUES ("debian", "preseed", "url", "http://${HOSTNAME}.${DOMAIN}/cmdb/", "$MIRROR", "auto=true priority=critical vga=788");
---INSERT INTO build_type (alias, build_type, arg, url, mirror, boot_line) VALUES ("ubuntu", "preseed", "url", "http://${HOSTNAME}.${DOMAIN}/cmdb/", "$MIRROR", "auto=true priority=critical vga=788");
---INSERT INTO build_type (alias, build_type, arg, url, mirror, boot_line) VALUES ("centos", "kickstart", "ks", "http://${HOSTNAME}.${DOMAIN}/cmdb/", "$MIRROR", "ksdevice=eth0 console=tty0 ramdisk_size=8192");
+
 --
 -- Create build_os entries for the above OS's
 INSERT INTO build_os (os, os_version, alias, ver_alias, arch, bt_id) SELECT "Debian", "7", alias, "wheezy", "i386", bt_id FROM build_type WHERE alias = "debian";
