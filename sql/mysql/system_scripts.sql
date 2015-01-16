4acb3028d7055e8939abe0cdde8f8f878ddac5b0
Iain M Conochie <iain@thargoid.co.uk>
2015-01-16 11:17:12 +0000

    Added mysql definitions for system_scripts* tables

---
+++ sql/mysql/system_scripts.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `system_scripts` (
+`systscr_id` int(7) NOT NULL AUTO_INCREMENT,
+`name` varchar(127) NOT NULL,
+`cuser` int(11) NOT NULL DEFAULT 0,
+`muser` int(11) NOT NULL DEFAULT 0,
+`ctime` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+`mtime` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+PRIMARY KEY (`systscr_id`)
+);
+
+CREATE TRIGGER system_scripts_update BEFORE UPDATE ON system_scripts FOR EACH ROW set NEW.mtime = NOW();
+CREATE TRIGGER system_scripts_insert BEFORE INSERT ON system_scripts FOR EACH ROW set NEW.mtime = NOW();
---
+++ sql/mysql/system_scripts_args.sql
@@ -0,0 +1,19 @@
+CREATE TABLE `system_scripts_args` (
+`systscr_arg_id` int(7) NOT NULL AUTO_INCREMENT,
+`systscr_id` int(11) NOT NULL,
+`arg` varchar(127) NOT NULL,
+`no` int(11) NOT NULL,
+`cuser` int(11) NOT NULL DEFAULT 0,
+`muser` int(11) NOT NULL DEFAULT 0,
+`ctime` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+`mtime` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+PRIMARY KEY (`systscr_arg_id`),
+KEY `systscr_id` (`systscr_id`),
+FOREIGN KEY(`systscr_id`)
+REFERENCES `system_scripts`(`systscr_id`)
+ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TRIGGER system_scripts_args_update BEFORE UPDATE ON system_scripts_args FOR EACH ROW set NEW.mtime = NOW();
+CREATE TRIGGER system_scripts_args_insert BEFORE INSERT ON system_scripts_args FOR EACH ROW set NEW.mtime = NOW();
+
