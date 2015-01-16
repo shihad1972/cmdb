b851736e922641573d7c9f3415a42d5dc06c806b
Iain M Conochie <iain@thargoid.co.uk>
2015-01-16 13:51:02 +0000

    Start of cbcscripts: [.c|.h] files and entries in Makefiles

---
+++ src/cbcscript.c
@@ -0,0 +1,50 @@
+/*
+ *
+ *  cbcscript: Create Build Configuration Scripts
+ *  Copyright (C) 2014 - 2015  Iain M Conochie <iain-AT-thargoid.co.uk>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ *  cbcscript.c
+ *
+ *  Functions to get configuration values and also parse command line arguments
+ *
+ *  Part of the cbcscript program
+ *
+ */
+#define _GNU_SOURCE
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+#include "cmdb.h"
+#include "cmdb_cbc.h"
+#include "cbc_data.h"
+#include "cbc_common.h"
+#include "base_sql.h"
+#include "cbc_base_sql.h"
+#include "checks.h"
+#include "cbcsysp.h"
+
+int
+main(int argc, char *argv[])
+{
+	int retval = 0;
+
+	return retval;
+}
+
