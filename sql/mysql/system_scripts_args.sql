0dd82cc5971d6f88df322bdc59996c1eda688342
Iain M Conochie <iain@thargoid.co.uk>
2015-01-17 15:53:37 +0000

    Updated database definitions to include bd_id

--- sql/mysql/system_scripts_args.sql
+++ sql/mysql/system_scripts_args.sql
@@ -1,6 +1,7 @@
 CREATE TABLE `system_scripts_args` (
 `systscr_arg_id` int(7) NOT NULL AUTO_INCREMENT,
 `systscr_id` int(11) NOT NULL,
+`bd_id` int(11) NOT NULL,
 `arg` varchar(127) NOT NULL,
 `no` int(11) NOT NULL,
 `cuser` int(11) NOT NULL DEFAULT 0,
@@ -9,8 +10,12 @@ CREATE TABLE `system_scripts_args` (
 `mtime` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
 PRIMARY KEY (`systscr_arg_id`),
 KEY `systscr_id` (`systscr_id`),
+KEY `bd_id` (`bd_id`),
 FOREIGN KEY(`systscr_id`)
 REFERENCES `system_scripts`(`systscr_id`)
+ON UPDATE CASCADE ON DELETE CASCADE,
+FOREIGN KEY(`bd_id`)
+REFERENCES `build_domain`(`bd_id`)
 ON UPDATE CASCADE ON DELETE CASCADE
 );
 
--- sql/sqlite/system_scripts_args.sql
+++ sql/sqlite/system_scripts_args.sql
@@ -1,6 +1,7 @@
 CREATE TABLE `system_scripts_args` (
 `systscr_arg_id` INTEGER PRIMARY KEY,
 `systscr_id` int(11) NOT NULL,
+`bd_id` int(11),
 `arg` varchar(127) NOT NULL,
 `no` int(11) NOT NULL,
 `cuser` int(11) NOT NULL DEFAULT 0,
@@ -9,6 +10,9 @@ CREATE TABLE `system_scripts_args` (
 `mtime` timestamp NOT NULL DEFAULT 0,
 FOREIGN KEY(`systscr_id`)
 REFERENCES `system_scripts`(`systscr_id`)
+ON UPDATE CASCADE ON DELETE CASCADE,
+FOREIGN KEY(`bd_id`)
+REFERENCES `build_domain`(`bd_id`)
 ON UPDATE CASCADE ON DELETE CASCADE
 );
 CREATE TRIGGER insert_system_scripts_args AFTER INSERT ON system_scripts_args
