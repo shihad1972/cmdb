0fd5b6d0631e2807707ef3d95b0dcaaab89a9395
Iain M Conochie <iain@thargoid.co.uk>
2015-08-26 12:45:23 +0100

    Added sssd-auth.sh

---
+++ scripts/sssd-auth.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+#
+# Script to build an ssd configuration file
+#
+# Parameters:
+#		-r REALM
+#		  Use kerberos REALM
+#		-b base-dn
+#		  Base ldap search dn
+#		-d domain
+#		  Domain name to use for DNS discovery
+#		-k
+#		  Do not setup kerberos authentication
+#
+# SRV records are required for ldap and kerberos related services in the domain
+#
+
+WGET=`which wget`
+while getopts "b:d:kr:u:" opt; do
+  case $opt in 
+    b)	BASE_DN=$OPTARG
+	;;
+    d)	DOMAIN=$OPTARG
+	;;
+    k)	KERBEROS=no	# Currently a place holder. Does nothing.
+	;;		# Kerberos is always configured
+    r)	REALM=$OPTARG
+	;;
+    u)	URL=$OPTARG
+	;;
+    \?) echo "Usage: $0 -b <base-dn> -d <domain name> [ -r <KRB5 REALM> ] [ -k ] [ -u <url-for-ssl-cert> ]"
+	exit 1
+  esac
+done
+
+# Sanity checks
+if [ -z "$DOMAIN" ]
+  echo "Must provide DNS domain"
+  exit 1
+fi
+
+if [ -z "$BASE_DN" ]
+  echo "Must provide a base DN"
+  exit 1
+fi
+
+if [ -z "$REALM" ]; then
+  REALM=`echo $DOMAIN | tr '[:lower:]' '[:upper:]'`
+  echo "Used domain $DOMAIN to create kerberos realm $REALM"
+fi
+
+# Check to see if we are running in a preseed install
+if [ -d /target ]; then
+  TARGET=/target/etc/sssd/sssd.conf
+  TGT=/target
+else
+  TARGET=/etc/sssd/sssd.conf
+  unset TGT
+fi
+
+echo "Creating $TARGET file"
+cat >$TARGET<<EOF
+[sssd]
+config_file_version = 2
+services = nss, pam, ssh, sudo
+domains = $DOMAIN
+
+[nss]
+
+[pam]
+
+[domain/$DOMAIN]
+id_provider = ldap
+auth_provider = krb5
+ldap_schema = rfc2307
+ldap_search_base = $BASE_DN
+dns_discovery_domain = $DOMAIN
+rkb5_realm = $REALM
+EOF
+
+if [ -n "$URL" ] && [ -n "$WGET" ]; then
+  $WGET $URL -O ${TGT}/etc/ssl/certs/Root-CA.pem
+  if [ -n $TGT ]; then
+    cat >> ${TGT}/usr/share/firstboot/001-rehash.sh <<EOF
+#!/bin/sh
+
+/usr/bin/c_rehash /etc/ssl/certs/
+EOF
+    chmod 755 ${TGT}/usr/share/firstboot/001-rehash.sh
+  else
+    /usr/bin/c_rehash /etc/ssl/certs/
+  fi
+fi
+
