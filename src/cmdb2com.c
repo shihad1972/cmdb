3a6cbd67f99a0a8eacc61c107957388c5c830c1c
Iain M Conochie <iain@thargoid.co.uk>
2018-03-19 15:42:44 +0000

    More additions to .gitignore; fixed typo in cmdb2com.c

--- src/cmdb2com.c
+++ src/cmdb2com.c
@@ -17,7 +17,7 @@
  *  with this program; if not, write to the Free Software Foundation, Inc.,
  *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  *
- *  cmdbcom.c
+ *  cmdb2com.c
  *
  *  Contains the functions to deal with command line arguments and also to
  *  read the values from the configuration file
