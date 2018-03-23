351c342ab8024fb7d71f12f27c1ae10fa1bf0e5a
Iain M Conochie <iain@thargoid.co.uk>
2018-03-23 15:10:22 +0000

    More added to mkvm

--- src/mkvm.c
+++ src/mkvm.c
@@ -23,11 +23,84 @@
  *
  */
 
+#include <config.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#ifdef HAVE_GETOPT_H
+# include <getopt.h>
+#endif // HAVE_GETOPT_H
+#include <ailsacmdb.h>
+
+static int
+parse_mkvm_command_line(int argc, char *argv[], ailsa_mkvm_s *vm);
+
 int
 main(int argc, char *argv[])
 {
 	int retval = 0;
+	ailsa_mkvm_s *vm = ailsa_calloc(sizeof(ailsa_mkvm_s), "vm in main");
 
+	if ((retval = parse_mkvm_command_line(argc, argv, vm)) != 0)
+		goto cleanup;
+
+	cleanup:
+		ailsa_show_error(retval);
+		ailsa_clean_mkvm((void *)vm);
+		return retval;
+}
+
+static int
+parse_mkvm_command_line(int argc, char *argv[], ailsa_mkvm_s *vm)
+{
+	int retval = 0;
+	int opt;
+	const char *optstr = "n:p:ahv";
+	size_t len;
+
+#ifdef HAVE_GOTOPT_H
+	int index;
+	struct option opts[] = {
+		{"name",	required_argument,	NULL,	'n'},
+		{"pool",	required_argument,	NULL,	'p'},
+		{"add",		no_argument,		NULL,	'a'},
+		{"help",	no_argument,		NULL,	'h'},
+		{"version",	no_argument,		NULL,	'v'}
+	};
+	while ((opt = getopt_long(argc, argv, optstr, opts, &index)) != -1)
+#else
+	while ((opt = getopt(argc, argv, optstr)) != -1)
+#endif // HAVE_GETOPT_H
+	{
+		switch(opt) {
+		case 'n':
+			if (strlen(optarg) >= DOMAIN_LEN)
+				fprintf(stderr, "hostname trimmed to 255 characters\n");
+			vm->name = strndup(optarg, DOMAIN_LEN);
+			break;
+		case 'p':
+			if (strlen(optarg) >= DOMAIN_LEN)
+				fprintf(stderr, "pool namd trimmed to 255 characters\n");
+			vm->pool = strndup(optarg, DOMAIN_LEN);
+			break;
+		case 'a':
+			vm->action = AILSA_ADD;
+			break;
+		case 'h':
+			vm->action = AILSA_HELP;
+			break;
+		case 'v':
+			vm->action = AILSA_VERSION;
+			break;
+		default:
+			fprintf(stderr, "Unknown option %c\n", opt);
+			retval = 1;
+			break;
+		}
+	}
+	if (vm->action == 0)
+		retval = AILSA_NO_ACTION;
 	return retval;
 }
 
