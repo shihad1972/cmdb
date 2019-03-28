e90f042df49ed170a3c001d43f0a979a0089da53
Iain M Conochie <iain@thargoid.co.uk>
2019-03-28 12:08:14 +0000

    Placeholder file for mksp (MaKe Storage Pool)

---
+++ src/mksp.c
@@ -0,0 +1,49 @@
+/*
+ *
+ *  mkvm : make virtual machine
+ *  Copyright (C) 2018 - 2019  Iain M Conochie <iain-AT-thargoid.co.uk>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ *  mkvm.c
+ *
+ *  main and command line parsing functions for mkvm
+ *
+ */
+
+#include <config.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#ifdef HAVE_GETOPT_H
+# include <getopt.h>
+#endif // HAVE_GETOPT_H
+#ifdef HAVE_REGEX_H
+# include <regex.h>
+#endif // HAVE_REGEX_H
+#ifdef HAVE_WORDEXP_H
+# include <wordexp.h>
+#endif // HAVE_WORDEXP_H
+#include <ailsacmdb.h>
+#include "virtual.h"
+
+int
+main(int argc, char *argv[])
+{
+	int retval = 0;
+
+	return retval;
+}
