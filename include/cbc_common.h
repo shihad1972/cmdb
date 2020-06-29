0d8b853a04607d8a8756e49979b58b04effe8ffc
Iain M Conochie <iain@thargoid.co.uk>
2020-06-29 16:28:08 +0100

    removed un-needed cbc functions

--- include/cbc_common.h
+++ include/cbc_common.h
@@ -29,66 +29,15 @@
 # include "cbc_data.h"
 
 int
-get_varient_id(ailsa_cmdb_s *cmc, char *vari, unsigned long int *varient_id);
-
-void 
-cbc_set_varient_updated(ailsa_cmdb_s *cbc, unsigned long int vid);
-
-void
-set_build_domain_updated(ailsa_cmdb_s *cbt, char *domain);
-
-int
-get_default_id(ailsa_cmdb_s *cbc, int query, char *name, unsigned long int *id);
-
-int
 get_server_id(ailsa_cmdb_s *cbc, char *server, unsigned long int *id);
-
-int
-get_os_id(ailsa_cmdb_s *cmc, char *os[], unsigned long int *os_id);
-
-int
-get_build_domain_id(ailsa_cmdb_s *cbc, char *domain, uli_t *id);
-
-int
-get_system_package_id(ailsa_cmdb_s *cbc, char *domain, uli_t *id);
-
-int
-get_syspack_arg_id(ailsa_cmdb_s *cbc, char *field, uli_t sp_id, uli_t *id);
-
-int
-get_system_script_id(ailsa_cmdb_s *cbc, char *package, uli_t *id);
-
-int
-get_build_type_id(ailsa_cmdb_s *cbc, char *os, uli_t *id);
 
 int
 get_partition_id(ailsa_cmdb_s *cbc, char *name, char *mount, uli_t *id);
 
 int
-get_scheme_id(ailsa_cmdb_s *cbc, char *name, uli_t *id);
-
-int
-get_locale_id(ailsa_cmdb_s *cbc, char *name, uli_t *id);
-
-int
 get_scheme_id_from_build(ailsa_cmdb_s *cbc, uli_t server_id, uli_t *id);
 
 int
-get_os_alias(ailsa_cmdb_s *cbc, char *os, char *alias);
-
-int
 get_scheme_name(ailsa_cmdb_s *cbc, uli_t server_id, char *name);
-
-int
-get_part_opt_id(ailsa_cmdb_s *cbc, char *name, char *part, char *opt, uli_t *id);
-
-int
-set_scheme_updated(ailsa_cmdb_s *cbc, char *scheme);
-
-void
-fill_dbdata_os_search(dbdata_s *data, char *os[]);
-
-int
-cbc_add_server(ailsa_cmdb_s *cbc, char *name, long unsigned int *server_id);
 
 #endif /* CBC_COMMON_H */
